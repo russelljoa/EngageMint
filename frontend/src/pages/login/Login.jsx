72a498c5eafe450eda426c09772e7311b3fb1e09
Patrick Fish <patrickfish10@icloud.com>
2025-04-26 13:03:02 -0400

    OAuth functionality

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,9 +1,32 @@
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { useGoogleLogin } from '@react-oauth/google';
 import './Login.css';
 
-const Login = () => {
+const Login = ({ setIsAuthenticated }) => {
+    const navigate = useNavigate();
+
+    const oauth = useGoogleLogin({
+        onSuccess: tokenResponse => {
+            console.log(tokenResponse);
+            const token = tokenResponse.access_token || tokenResponse.credential;
+            sessionStorage.setItem('authToken', token);
+            setIsAuthenticated(true);
+            navigate('/home');
+        },
+        onError: error => {
+            console.error('Login failed', error);
+        }
+    });
+
     return (
-        <h1>Login</h1>
+        <div className="login_container">
+            Hello World
+            <button className="login_button" onClick={oauth}>
+                {/* <img className="login_googleLogo" src={GoogleLogo} alt="Google Logo" /> */}
+                Login with Google
+            </button>
+        </div>
     )
 }
 
