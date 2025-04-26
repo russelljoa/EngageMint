9c83fe92b0ef9744c4b5fb8e387095a88d260e5f
Patrick Fish <patrickfish10@icloud.com>
2025-04-26 14:00:21 -0400

    adds users to db

---
+++ backend/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const { MongoClient } = require('mongodb');
+
+const app = express();
+dotenv.config();
+app.use(cors());
+app.use(bodyParser.json());
+
+const MONGO_URI = process.env.MONGO_URI;
+const client = new MongoClient(MONGO_URI);
+
+const PORT = process.env.PORT || 5000;
+
+app.post('/inputuser', async (req, res) => {
+    const userInput = req.body;
+    try {
+        const db = client.db('EngageMint');
+        const collection = db.collection('users');
+        const inputData = {
+            email: userInput.email,
+            wallet: "",
+        };
+        const existingUser = await collection.findOne({ email: userInput.email });
+        if (!existingUser) {
+            await collection.insertOne(inputData);
+        }
+        res.status(200).json({ message: 'Input received successfully' });
+    } catch (error) {
+        console.error('Error inserting data:', error);
+        res.status(500).json({ message: 'Error inserting data' });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
